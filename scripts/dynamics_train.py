d9f0c60f727797b93232a6e71970602c0e543044
Matin Moezzi <matin.moezzi@outlook.com>
2023-08-09 13:38:35 -0400

    bug fixes

--- scripts/dynamics_train.py
+++ scripts/dynamics_train.py
@@ -12,23 +12,6 @@ from dynamics_diffusion import dist_util, logger
 from dynamics_diffusion.train_util import SDETrainLoop
 
 
-def sde_train(score_model, sde, data, data_info, **kwargs):
-    score_model_cfg = score_model
-
-    state_dim = data_info["state_dim"]
-    action_dim = data_info["action_dim"]
-    cond_dim = state_dim + action_dim
-
-    logger.log("creating score model and sde...")
-    sde = hydra.utils.instantiate(sde.target)
-    score_model = hydra.utils.instantiate(score_model_cfg.target, state_dim, cond_dim)
-    score_model.to(dist_util.dev())
-
-    logger.log("training...")
-    SDETrainLoop(score_model=score_model, sde=sde, data=data, **kwargs).run_loop()
-    pass
-
-
 @hydra.main(version_base=None, config_path="../config", config_name="train_config.yaml")
 def main(cfg: DictConfig):
     log_dir = Path(HydraConfig.get().run.dir, "train").resolve()
