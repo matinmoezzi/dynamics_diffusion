001f7c35ee287d242f1209757ca9ed1d23e508c7
Matin Moezzi <matin.moezzi@outlook.com>
2023-10-11 11:37:22 -0700

    bug fixes

--- scripts/dynamics_train.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-~/miniforge3/bin/conda init
-source ~/.bashrc
-
-cd ~/matin/dynamics_diffusion
-conda activate dynamics_diffusion
-
-python scripts/dynamics_train.py trainer.total_training_steps=42000
