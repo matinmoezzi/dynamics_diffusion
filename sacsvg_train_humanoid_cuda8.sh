d85de21914a84d75a21d1f080667bbb837d319a2
Matin Moezzi <matin.moezzi@outlook.com>
2023-10-30 14:42:41 -0700

    add ant tasks

---
+++ sacsvg_train_humanoid_cuda8.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+set -x
+
+torchrun --nproc_per_node=8 --nnodes=1 --node_rank=0 --rdzv_id=123 --rdzv_backend=c10d --rdzv_endpoint=localhost:22411 scripts/train_sacsvg.py env=mbpo_humanoid dx=$1 num_train_steps=$2
--- sacsvg_train_pendulum_seqdx_5k_cuda8.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-set -x
-
-torchrun --nproc_per_node=8 --nnodes=$1 --node_rank=$2 --rdzv_id=123 --rdzv_backend=c10d --rdzv_endpoint=localhost:22491 scripts/train_sacsvg.py env=pendulum dx=seqdx num_train_steps=5000
