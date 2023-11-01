7d1e5beef4d150e125d434ebfb91bd2243d8993b
Matin Moezzi <matin.moezzi@outlook.com>
2023-10-31 17:29:11 -0700

    update bash scripts

--- sacsvg_train_ant_cuda8.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-set -x
-
-torchrun --nproc_per_node=8 --nnodes=1 --node_rank=0 --rdzv_id=123 --rdzv_backend=c10d --rdzv_endpoint=localhost:22411 scripts/train_sacsvg.py env=mbpo_ant dx=$1 num_train_steps=$2
